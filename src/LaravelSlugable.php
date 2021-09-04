92de5af960456e46b8953c99b63f51bae1e43512
Aleksey Popov <7996096@gmail.com>
2021-09-04 15:37:25 +0300

    initial commit

---
+++ src/LaravelSlugable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zyxus\LaravelSluggable\LaravelSluggable;
+
+trait LaravelSluggable
+{
+    public function setSlug(string $slug)
+    {
+        dd('setSlug called');
+    }
+
+    public function getSlug()
+    {
+        dd('getSlug called');
+    }
+
+    public function listSlugs()
+    {
+        dd('listSlugs called');
+    }
+}
