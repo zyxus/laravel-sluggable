81a876318103ed4da3b312dd6ec02de77aa4dcce
Aleksey Popov <7996096@gmail.com>
2021-09-05 16:29:48 +0300

    - service provider

--- src/SluggableServiceProvider.php
+++ src/SluggableServiceProvider.php
@@ -6,16 +6,23 @@ use Illuminate\Support\ServiceProvider;
 
 class SluggableServiceProvider extends ServiceProvider
 {
-  public function boot()
-  {
-    if ($this->app->runningInConsole()) {
-      // Export the migration
-      if (! class_exists('CreateSlugsTable')) {
-        $this->publishes([
-          __DIR__ . '/../database/migrations/create_slugs_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_slugs_table.php'),
-          // you can add any number of migrations here
-        ], 'migrations');
-      }
+
+
+    public function register()
+    {
+        //
     }
-  }
+
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            // Export the migration
+            if (! class_exists('CreateSlugsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_slugs_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_slugs_table.php'),
+                    // you can add any number of migrations here
+                ], 'migrations');
+            }
+        }
+    }
 }
