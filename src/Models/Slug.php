17921b754341cd3e6c3d2eaec85e545d261a1890
Aleksey Popov <7996096@gmail.com>
2021-09-18 12:32:45 +0300

    Связь с моделью

--- src/Models/Slug.php
+++ src/Models/Slug.php
@@ -3,12 +3,20 @@
 namespace Zyxus\LaravelSluggable\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Slug extends Model
 {
+    protected $table = "slugs";
+
     public $fillable = [
         'model_type',
         'model_id',
         'slug'
     ];
+
+    public function model()
+    {
+        return $this->morphTo('model');
+    }
 }
--- src/Sluggable.php
+++ src/Sluggable.php
@@ -2,41 +2,30 @@
 
 namespace Zyxus\LaravelSluggable;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Zyxus\LaravelSluggable\Models\Slug;
 
 trait Sluggable
 {
-    private function getModel()
+    public static function getBySlug(string $slug)
     {
-        $model = (new \ReflectionClass($this))->getName();
-        $model = str_replace('App\Models', '', $model);
-        return $model;
+        $slug = Slug::where('slug', $slug)->first();
+        return $slug->model;
     }
 
     public function setSlug(string $slug)
     {
         $sluggable = new Slug();
-        $sluggable->__set('model_type', $this->getModel());
+        $sluggable->__set('model_type', self::class);
         $sluggable->__set('model_id', $this->id);
         $sluggable->__set('slug', $slug);
         $sluggable->save();
-    }
 
-    public function getSlugById()
-    {
-        $slug = Slug::find($this->id);
-        return $slug->slug;
+        return $this;
     }
 
     public function getSlug()
     {
-        $slug = Slug::find($this->id);
-        return $slug->slug;
-    }
-
-   public function getSlug()
-    {
-        $slug = Slug::find($this->id);
-        return $slug->slug;
+        return Slug::where('model_type', $this->getModel())->where('model_id', $this->id)->get();
     }
 }
